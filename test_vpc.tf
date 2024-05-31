bbac08e5039edd38ca559f2a09e9036b508c599c
Ryan Weber <rweber12@emich.edu>
2024-05-31 14:50:31 -0400

    update name

--- test_vpc.tf
+++ test_vpc.tf
@@ -1,9 +1,9 @@
-resource "aws_vpc" "acloudsecninja-cloudsec-vpc-test" {
+resource "aws_vpc" "acloudsecninja-cloudsec-vpc-test2" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   tags = {
-    Name = "acloudsecninja-cloudsec-vpc-test"
+    Name = "acloudsecninja-cloudsec-vpc-test2"
   }
 }
