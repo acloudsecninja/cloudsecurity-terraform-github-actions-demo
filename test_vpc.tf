844e8cad29f93f9dd168c92cb30f2568c0ae97df
acloudsecninja <rweber12@emich.edu>
2024-12-06 16:06:35 -0500

    update resources

--- test_vpc.tf
+++ test_vpc.tf
@@ -11,12 +11,11 @@ resource "aws_vpc" "acloudsecninja-cloudsec-vpc-test2" {
 
 
 ## GOOD Resource ##
-#resource "aws_vpc" "acloudsecninja-cloudsec-vpc-test2" {
-#  cidr_block           = "10.0.0.0/16"
-#  enable_dns_hostnames = true
-#  enable_dns_support   = true
+resource "aws_s3_bucket" "example123412312" {
+  bucket = "example123412312"
 
-#  tags = {
-#    Name = "acloudsecninja-cloudsec-vpc-test2"
-#  }
-#}
+  tags = {
+    Name        = "example123412312"
+    Environment = "nprd"
+  }
+}
