b4d0d0e4313ca07e3b079820816097740d83640d
acloudsecninja <rweber12@emich.edu>
2024-12-06 15:57:47 -0500

    remove old flow

--- test_vpc.tf
+++ test_vpc.tf
@@ -1,3 +1,4 @@
+## BAD Resource ##
 resource "aws_vpc" "acloudsecninja-cloudsec-vpc-test2" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
@@ -7,3 +8,15 @@ resource "aws_vpc" "acloudsecninja-cloudsec-vpc-test2" {
     Name = "acloudsecninja-cloudsec-vpc-test2"
   }
 }
+
+
+## GOOD Resource ##
+#resource "aws_vpc" "acloudsecninja-cloudsec-vpc-test2" {
+#  cidr_block           = "10.0.0.0/16"
+#  enable_dns_hostnames = true
+#  enable_dns_support   = true
+
+#  tags = {
+#    Name = "acloudsecninja-cloudsec-vpc-test2"
+#  }
+#}
